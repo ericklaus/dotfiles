afbb06f8347903ea30800f1dabb900f0b264d94b
Eric Klaus <eric.klaus@workiva.com>
2022-06-13 16:01:14 -0500

    Add 1Password to setup packages

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -2,6 +2,7 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+command -v op >/dev/null || brew install --cask 1password/tap/1password-cli
 command -v fish >/dev/null || brew install fish
 command -v asdf >/dev/null || brew install asdf
 command -v starship >/dev/null || brew install starship
