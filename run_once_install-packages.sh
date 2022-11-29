8dbd4e6417275a5bf0bce13f45efa0573df933f1
Eric Klaus <eric.klaus@workiva.com>
2022-11-29 15:53:53 -0600

    Correctly install direnv for asdf

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -6,4 +6,9 @@ command -v op >/dev/null || brew install --cask 1password/tap/1password-cli
 command -v fish >/dev/null || brew install fish
 command -v asdf >/dev/null || brew install asdf
 command -v starship >/dev/null || brew install starship
-asdf plugin list | grep -q direnv || asdf plugin-add direnv
+if ! asdf plugin list | grep -q direnv; then
+	asdf plugin-add direnv
+	asdf install direnv latest
+	asdf global direnv latest
+fi
+
