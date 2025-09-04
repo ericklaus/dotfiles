46152bfec6c53dc2081f69cea2b19bc97126b0df
Eric Klaus <etklaus@gmail.com>
2025-09-04 17:30:05 -0500

    Personal computer improvements

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -4,11 +4,5 @@ command -v brew >/dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubuserc
 
 command -v op >/dev/null || brew install --cask 1password/tap/1password-cli
 command -v fish >/dev/null || brew install fish
-command -v asdf >/dev/null || brew install asdf
 command -v starship >/dev/null || brew install starship
-if ! asdf plugin list | grep -q direnv; then
-	asdf plugin-add direnv
-	asdf install direnv latest
-	asdf global direnv latest
-fi
 
