a91dd0dc71034c1a1d9a2544324f0027b07dbf4e
Eric Klaus <eric.klaus@workiva.com>
2025-09-06 20:40:06 -0500

    iTerm settings

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -5,4 +5,6 @@ command -v brew >/dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubuserc
 command -v op >/dev/null || brew install --cask 1password/tap/1password-cli
 command -v fish >/dev/null || brew install fish
 command -v starship >/dev/null || brew install starship
-
+brew install font-symbols-only-nerd-font
+brew install --cask iterm2
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string 
